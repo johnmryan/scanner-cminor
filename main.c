d40df1ed2c3394088275d88b53ae37363637cd56
Jack Ryan <johnmryan830@gmail.com>
2015-09-18 02:20:29 -0400

    More done

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include "token.h"
 
 extern FILE* yyin;
 extern char* yytext;
 extern int yylex(void);
+
+char* removeQuotes(char* s);
+char temp[256];
 
 int main(int argc, char **argv) {
 	//enough arguments
@@ -17,8 +21,9 @@ int main(int argc, char **argv) {
 	token_t t;
 	while (1) {
 		t = yylex();
+		//if a string literal, print val and continue through loop
 		if (t == TOKEN_STRING_LITERAL) {
-			printf("%s %s\n", token_string(t), yytext);
+			printf("%s %s\n", token_string(t), removeQuotes(yytext));
 			continue;
 		}
 		if (!t) break;
@@ -26,4 +31,12 @@ int main(int argc, char **argv) {
 		//printf("%s\n", yytext);
 	}
 }
-	
+
+char* removeQuotes(char* s) {
+  int i = 1;
+  for (i; i < strlen(s)-1; i++) {
+  	temp[i-1] = s[i];
+  }
+  temp[strlen(temp)-1] = '\0';
+ 	return temp;
+}
